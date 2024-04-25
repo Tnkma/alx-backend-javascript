6d9204011ea4cb00494c5d174e637d6a3ce2275f
Tnkma <onwusilikegodswill@gmail.com>
2024-04-25 12:35:37 +0100

    lets make some rroms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+// Import the ClassRoom class from 0-classroom.js
+import ClassRoom from './0-classroom';
+
+// Define the method
+export default function initializeRooms() {
+  const roomSize = [19, 20, 34];
+  // initialize an array to store the classRoom
+  const rooms = [];
+
+  for (const size of roomSize) {
+    rooms.push(new ClassRoom(size));
+  }
+  return rooms;
+}
