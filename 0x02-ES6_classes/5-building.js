b8cbb0ba108671ff6cbc33517099a4eaade6bd35
Tnkma <onwusilikegodswill@gmail.com>
2024-04-26 22:15:20 +0100

    making of classess

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,19 @@
-export default abstract class Building {
-    protected _sqft: number;
-  
-    constructor(sqft: number) {
-      this._sqft = sqft;
-  
-      // Check if subclass overrides evacuationWarningMessage method
-      if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    // Check if subclass overrides evacuationWarningMessage method
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    // Abstract method to be implemented by subclasses
-    abstract evacuationWarningMessage(): string;
+  }
 
-    get sqft(): number {
-      return this._sqft;
-    }
+  // Abstract method (cannot be implemented in the parent class)
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('evacuationWarningMessage() must be implemented by subclasses');
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
