497fb2a54cbe1898b9d6c6488fa2f83d0b7f4e68
Tnkma <onwusilikegodswill@gmail.com>
2024-05-05 09:05:30 +0100

    get locations using filters

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
   const itemList = [];
 
-  const obj1 = { name: 'Guillaume', id: 1, location: 'San Francisco' };
-  const obj2 = { name: 'James', id: 2, location: 'Columbia' };
-  const obj3 = { name: 'Serena', id: 5, location: 'San Francisco' };
+  const obj1 = { firstname: 'Guillaume', id: 1, location: 'San Francisco' };
+  const obj2 = { firstname: 'James', id: 2, location: 'Columbia' };
+  const obj3 = { firstname: 'Serena', id: 5, location: 'San Francisco' };
 
   itemList.push(obj1, obj2, obj3);
   return itemList;
