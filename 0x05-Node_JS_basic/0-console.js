92fc23af3adccea17fc3233d26b9f9664a8acbb4
Godswill <onwusilikegodswill@gmail.com>
2024-06-18 23:00:04 +0100

    printed to STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message to the STDOUT
+ * @param {message} the message to be printed to the STDOUT
+ * @returns {void} 
+ */
 const displayMessage = (message) => {
     console.log(message)
 }
