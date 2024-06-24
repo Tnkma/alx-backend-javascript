bd0a991f977c805ff18c4c655127fe95437bf4fa
Godswill <onwusilikegodswill@gmail.com>
2024-06-24 17:58:57 +0100

    fixing non interactive response

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,14 @@
  * stdout: standard output
  */
 const process = require('process');
+const tty = require('tty');
 
 const welcomeMessage = 'Welcome to Holberton School, what is your name? \n';
 const closeMessage = 'This important software is now closing\n';
 
+const isInput = tty.isatty(process.stdin.fd);
+
+// if the input is a tty we collect the name and print it
 const handleEnd = () => {
   console.log(closeMessage);
   // ensure we close perfectly
@@ -16,17 +20,27 @@ const handleEnd = () => {
 const handleInput = (name) => {
   console.log(`Your name is: ${name}`);
   process.exit(0);
-  // handleEnd();
 };
 
-process.stdout.write(welcomeMessage);
-// listen for data on the standard input
-process.stdin.resume();
+const handleStreamput = (data) => {
+  console.log(`Your name is: ${data}`);
+  handleEnd();
+};
 
-// read and trim the data from stdin
-process.stdin.on('data', (data) => {
-  handleInput(data.toString().trim());
-});
+if (isInput) {
+  // if the input is a tty we print the welcome message
+  process.stdout.write(welcomeMessage);
+  process.stdin.on('data', (data) => {
+    handleInput(data.toString().trim());
+  });
+} else {
+  process.stdout.write(welcomeMessage);
+  // listen for data on the standard input
+  process.stdin.resume();
+  process.stdin.on('data', (data) => {
+    handleStreamput(data.toString().trim());
+  });
+}
 
 // Handle termination by CTRL+C
 process.on('SIGINT', handleEnd);
