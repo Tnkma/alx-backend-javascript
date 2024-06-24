eb973b3c65f8d08e15fdff165831789db83beb6b
Godswill <onwusilikegodswill@gmail.com>
2024-06-24 22:11:40 +0100

    Reading a file with nodejs

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const fields = {};
+    lines.slice(1).forEach(line => {
+      const [firstname, , , field] = line.split(',');
+      if (!firstname || !field) return;
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    const totalStudents = Object.values(fields).reduce((acc, curr) => acc + curr.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
