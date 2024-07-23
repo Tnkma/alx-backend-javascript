785c9ce96757e05cf032b9710d742c4a19fefcc5
Godswill <onwusilikegodswill@gmail.com>
2024-07-23 21:11:57 +0100

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,31 +1,20 @@
-/**
- * stdin: standard input
- * stdout: standard output
- */
 const process = require('process');
 const tty = require('tty');
 
-const welcomeMessage = 'Welcome to Holberton School, what is your name? \n';
+const welcomeMessage = 'Welcome to Holberton School, what is your name?\n';
 const closeMessage = 'This important software is now closing\n';
 
 const isInput = tty.isatty(process.stdin.fd);
 
 const main = () => {
-  if (isInput) {
-    process.stdout.write(welcomeMessage);
-    process.stdin.on('data', (data) => {
-      console.log(`Your name is: ${data.toString().trim()}`);
-      process.exit(0);
-    });
-  } else {
-    process.stdout.write(welcomeMessage);
-    process.stdin.resume();
-    process.stdin.on('data', (data) => {
-      console.log(`Your name is: ${data.toString().trim()}`);
+  process.stdout.write(welcomeMessage);
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+    if (!isInput) {
       process.stdout.write(closeMessage);
-      process.exit(0);
-    });
-  }
+    }
+    process.exit(0);
+  });
 };
 
 main();
