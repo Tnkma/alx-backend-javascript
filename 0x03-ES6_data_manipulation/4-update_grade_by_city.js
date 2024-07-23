5b4929bf920c1305921d729e0dc0fa3ce333c2fe
Godswill <onwusilikegodswill@gmail.com>
2024-07-23 01:46:51 +0100

    :test_tube:was using wrong method to get students list

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,14 @@
 // Import getListStudentIds function from './1-get_list_student_ids'
-import getListStudentIds from './1-get_list_student_ids';
+import getListStudents from './0-get_list_students';
 
 // Define the updateStudentGradeByCity function
 export default function updateStudentGradeByCity(studentIds, city, newGrades) {
   // Get the list of students based on studentIds using getListStudentIds
-  const students = getListStudentIds(studentIds);
+  const students = getListStudents(studentIds);
 
   // Filter students by the specified city
-  const studentsInCity = students.filter((student) => student.city === city);
+  const studentsInCity = students.filter((student) => student.location === city);
+  console.log(studentsInCity);
 
   // Map through the students in the city and update their grades
   const updatedStudents = studentsInCity.map((student) => {
@@ -23,7 +24,6 @@ export default function updateStudentGradeByCity(studentIds, city, newGrades) {
       grade: updatedGrade,
     };
   });
-
   // Return the array of students with updated grades
   return updatedStudents;
 }
