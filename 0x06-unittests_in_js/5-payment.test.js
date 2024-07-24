2677b2a0059a7241b925c34adff3a3ab8eab909e
Godswill <onwusilikegodswill@gmail.com>
2024-07-24 14:25:15 +0100

    using hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,29 +1,31 @@
 const sinon = require('sinon');
-const chai = require('chai');
-const { sendPaymentRequestToApi } = require('./4-payment.js');
-
-const { expect } = chai;
+const { expect } = require('chai'); // Correct import statement
+const sendPaymentRequestToApi = require('./4-payment.js'); // Make sure this path is correct
 
 describe('sendPaymentRequestToApi', function () {
-  let spy;
+  let spyConsole;
 
   beforeEach(function () {
-    // Spy on console.log to verify the output
-    spy = sinon.spy(console, 'log');
+    // Create a spy on console.log
+    spyConsole = sinon.spy(console, 'log');
   });
 
   afterEach(function () {
-    // Restore the spy
-    spy.restore();
+    // Restore the original console.log
+    spyConsole.restore();
   });
 
   it('should log "The total is: 120" when called with 100 and 20', function () {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
   });
 
   it('should log "The total is: 20" when called with 10 and 10', function () {
     sendPaymentRequestToApi(10, 10);
-    expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
   });
 });
